c6841701cfe1cdecc201f5d5df99f0ef07c55155
nrthbnd <asoloshenkova@gmail.com>
2023-10-25 17:59:31 +0300

    init commit

---
+++ scrapy_drugstore/items.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class ScrapyDrugstoreItem(scrapy.Item):
+    """Определить данные. извлекаемые при парсинге."""
+    timestamp = scrapy.Field()
+    RPC = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    marketing_tags = scrapy.Field()
+    section = scrapy.Field()
